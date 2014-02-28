3ccd33232005b759504a88cbe398d2287b3d131a
Felipe Álvarez <falvarez@votainteligente.cl>
2014-02-28 18:40:36 -0300

    Displaying home

--- banco/settings.py
+++ banco/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
+    'dona_sangre',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,6 +76,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 
 # Static files (CSS, JavaScript, Images)
---
+++ dona_sangre/tests/home_tests.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
+
+
+class HomeTestCase(TestCase):
+	def setUp(self):
+		self.client = Client()
+
+	def test_get_home_and_render(self):
+		"""Obtiene la pagina de incio y la dibuja bacán"""
+		url = reverse('home')
+		response = self.client.get(url)
+		self.assertEquals(response.status_code, 200)
+		self.assertTemplateUsed(response, 'dona_sangre/base.html')
